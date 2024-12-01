97936401f80c46f681da37ff77f96ad917f50032
Justin Mach <bachmach24k@gmail.com>
2024-12-01 13:00:12 -0600

    add: bootleg solution for PlayScreen navigation

--- app/src/main/java/com/team/hogspot/Navigation/Screen.kt
+++ app/src/main/java/com/team/hogspot/Navigation/Screen.kt
@@ -5,13 +5,13 @@ sealed class Screen(val route: String) {
     object LandingScreen : Screen("landing_screen")
     object LoginScreen : Screen("login_screen")
     object SignupScreen : Screen("signup_screen")
-    object ExploreScreen : Screen("explore_screen")
+    object ExploreScreen : Screen("explore_sceen")
     object SearchScreen : Screen("search_screen")
     object UserScreen : Screen("user_screen")
     object DetailedSpotScreen : Screen("detailed_spot_screen")
     object NewSpotScreen : Screen("add_spot_screen")
 
-    object PlayMapScreen : Screen("play_map_screen")
+    object PlayScreen : Screen("play_screen")
     object PlayDetailScreen : Screen("play_detail_screen")
 
     fun withArgs(vararg args: String): String {
